01082008abc9b56fa6c825c635cea77bb9ae5ef2
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-07-07 18:16:41 +0000

    do step11 of phonebook backend

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 
 // morgan request logging - adds req method for post requests
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-content', {
