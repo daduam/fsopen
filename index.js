86cc5f84e70165bc6fa5ac5dd949cb8b86a671f1
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-07-07 15:32:15 +0000

    do step9 of phonebook backend :heavy_plus_sign:
    Install and use cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 
 morgan.token('post-content', (req, res) => {
@@ -8,6 +9,7 @@ morgan.token('post-content', (req, res) => {
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // morgan request logging - adds req method for post requests
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-content', {
