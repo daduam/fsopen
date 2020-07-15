fa37a007cd2a4022625a23f827eedb86ae1cca08
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-07-15 16:00:07 +0000

    Add length validation for name and number

--- models/entry.js
+++ models/entry.js
@@ -20,10 +20,12 @@ const entrySchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minlength: 3,
     unique: true
   },
   number: {
     type: String,
+    minlength: 8
     required: true
   }
 });
