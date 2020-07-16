ac3291cdac428632f523bb5430ae558035a6c290
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-07-16 20:04:42 +0000

    Setup basic server function

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const { MONGODB_URI } = process.env
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('connected to db'))
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('<h1>Up and Awake</h1>')
+app.get('/api/blogs', (req, res) => {
+  Blog.find({})
+    .then(blogs => {
+      res.json(blogs)
+    })
 })
 
-const PORT = 3003
+app.post('/api/blogs', (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog.save()
+    .then(result => {
+      res.status(201).json(result)
+    })
+})
+
+const { PORT } = process.env
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
