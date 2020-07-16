50bb88930d7c9d50d16cdbe39711399d4db83cfb
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-07-16 19:30:55 +0000

    Install cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
-
 const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('<h1>Up and Awake</h1>')
