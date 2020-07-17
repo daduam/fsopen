54ed439217bb56ce04307805ae1c6b0108b1f4eb
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-07-17 00:49:30 +0000

    Add some list helpers

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,27 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+  const favorite = blogs.find(blog => blog.likes === maxLikes)
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
