4fd36f2c8d5975b090745913f1b4ed1b4e974224
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-07-20 14:59:42 +0000

    Add new routes and validation

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,20 +1,42 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (req, res) => {
-  Blog.find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
+blogsRouter.get('/', async (req, res) => {
+  const blogs = await Blog.find({})
+  res.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', (req, res) => {
+blogsRouter.post('/', async (req, res) => {
   const blog = new Blog(req.body)
 
-  blog.save()
-    .then(result => {
-      res.status(201).json(result)
-    })
+  const savedBlog = await blog.save()
+  res.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (req, res) => {
+  const result = await Blog.findByIdAndDelete(req.params.id)
+  res.status(204).end()
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+  const { body } = req;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    // TODO: Need to check against undefined likes
+    likes: body.likes
+  }
+
+  const opts = {
+    new: true,
+    runValidators: true,
+    context: 'query'
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, opts)
+  res.json(updatedBlog.toJSON())
 })
 
 module.exports = blogsRouter
