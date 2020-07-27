f7ef5f331fc2280e388ac570e44b04b9101f5db4
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-07-27 00:49:18 +0000

    Add view/hide details toggle

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [toggleLabel, setToggleLabel] = useState('view')
+
+  const toggleDetails = () => {
+    setToggleLabel(toggleLabel === 'view' ? 'hide' : 'view')
+  }
+  
+  const blogStyle = {
+    padding: '10px 2px',
+    border: 'solid',
+    borderWidth: '1px',
+    width: '80vw',
+    margin: '5px auto'
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} - {blog.author}
+        <button onClick={toggleDetails}>{toggleLabel}</button>
+      </div>
+      {toggleLabel === 'hide' && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}
+            <button>like</button>
+          </div>
+          <div>{blog.user ? blog.user.name : 'No User'}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
