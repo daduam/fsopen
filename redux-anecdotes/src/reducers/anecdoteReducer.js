345c934d4e28eabeec1d6f4e566defa68528233c
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-08-13 10:53:23 +0000

    anecdotes can be voted on

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -17,13 +17,29 @@ const asObject = (anecdote) => {
   }
 }
 
+export const voteWithId = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
+}
+
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.data.id
+      const anecdoteToVote = state.find(anecdote => anecdote.id === id)
+      const changedAnecdote = {
+        ...anecdoteToVote,
+        votes: anecdoteToVote.votes + 1
+      }
+      return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+      
+    default:
+      return state
+  }
 }
 
 export default reducer
