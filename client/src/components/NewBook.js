e53453e53f1c935b73016e4c6ebd45a3babec511
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-08-26 14:18:44 +0000

    new books can be added

--- client/src/components/NewBook.js
+++ client/src/components/NewBook.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ADD_BOOK, FETCH_AUTHORS, FETCH_BOOKS } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
-  const [author, setAuhtor] = useState('')
+  const [author, setAuthor] = useState('')
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+
+  const [addBook] = useMutation(ADD_BOOK, {
+    refetchQueries: [{ query: FETCH_AUTHORS }, { query: FETCH_BOOKS }],
+    onError: (error) => {
+      console.log(error)
+    }
+  })
 
   if (!props.show) {
     return null
@@ -13,12 +22,19 @@ const NewBook = (props) => {
 
   const submit = async (event) => {
     event.preventDefault()
-    
-    console.log('add book...')
+
+    addBook({
+      variables: {
+        title,
+        author,
+        published: Number(published),
+        genres
+      }
+    })
 
     setTitle('')
     setPublished('')
-    setAuhtor('')
+    setAuthor('')
     setGenres([])
     setGenre('')
   }
@@ -42,7 +58,7 @@ const NewBook = (props) => {
           author
           <input
             value={author}
-            onChange={({ target }) => setAuhtor(target.value)}
+            onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
         <div>
--- client/src/queries.js
+++ client/src/queries.js
@@ -21,3 +21,24 @@ export const FETCH_BOOKS = gql`
     }
   }
 `
+
+export const ADD_BOOK = gql`
+  mutation addBook(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      title
+      author
+      published
+      genres
+    }
+  }
+`
