1b280a907c67197ee79b549ffcfb5eb17289fece
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-08-27 10:55:11 +0000

    fix resolvers

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql, UserInputError } = require('apollo-server')
 const mongoose = require('mongoose')
 const Book = require('./models/book')
 const Author = require('./models/author')
@@ -7,9 +7,9 @@ const Author = require('./models/author')
 mongoose.set('useFindAndModify', true)
 
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  })
-  .then(() => console.log('🗄️  connected to MongoDB'))
-  .catch(error => console.error('error connecting to MongoDB', error.message))
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(error => console.error('Error connecting to MongoDB', error.message))
 
 const typeDefs = gql`
   type Book {
@@ -56,49 +56,49 @@ const resolvers = {
     bookCount: () => Book.estimatedDocumentCount(),
     authorCount: () => Author.estimatedDocumentCount(),
     allBooks: (parent, args) => {
-      if (!args.author && !args.genre) {
-        return books
+      if (!args.genre) {
+        return Book.find({}).populate('author')
       }
-      else if (args.author && !args.genre) {
-        return books.filter(b => b.author === args.author)
-      }
-      else if (args.genre && !args.author) {
-        return books.filter(b => b.genres.includes(args.genre))
-      }
-      return books.filter(b => (
-        b.author === args.author && b.genres.includes(args.genre)
-      ))
+      return Book.find({ genres: { $in: args.genre } }).populate('author')
     },
     allAuthors: () => Author.find({})
   },
   Author: {
     bookCount: (parent) => {
-      let count = 0
-      books.forEach(b => {
-        if (b.author === parent.name) {
-          count++
-        }
-      })
-      return count
+      return Book.countDocuments({ author: parent.id })
     }
   },
   Mutation: {
-    addBook: (parent, args) => {
-      const book = { ...args, id: uuidv1() }
-      books = books.concat(book)
-      if (!authors.find(a => a.name === args.author)) {
-        authors = authors.concat({ name: args.author, id: uuidv1() })
+    addBook: async (parent, args) => {
+      const author = await Author.findOne({ name: args.author })
+      const book = new Book({ ...args, author: author ? author.id : '' })
+
+      try {
+        if (!author) {
+          book.author = (await (new Author({ name: args.author })).save()).id
+        }
+        await book.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args
+        })
       }
+
       return book
     },
-    editAuthor: (parent, args) => {
-      const author = authors.find(a => a.name === args.name)
-      if (!author) {
-        return null
+    editAuthor: async (parent, args) => {
+      const author = await Author.findOne({ name: args.name })
+
+      try {
+        author.born = args.setBornTo
+        await author.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args
+        })
       }
-      const updatedAuthor = { ...author, born: args.setBornTo }
-      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
-      return updatedAuthor
+
+      return author
     }
   }
 }
