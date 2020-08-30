65e7232703ae6fa3e4983fe0e751942292038a77
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-08-30 11:46:45 +0000

    setup wslink

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  split
+} from '@apollo/client'
 import { setContext } from 'apollo-link-context'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('graphql-library-user')
@@ -15,11 +23,30 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000/graphql',
+  options: { reconnect: true }
+})
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink)
+  link: splitLink
 })
 
 ReactDOM.render(
