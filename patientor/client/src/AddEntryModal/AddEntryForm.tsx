ce2b4d94ff5bdb402af286dd7b2b36bda8fc6b8a
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-09-09 22:31:04 +0000

    not even close

---
+++ patientor/client/src/AddEntryModal/AddEntryForm.tsx
@@ -0,0 +1,93 @@
+import { Field, Form, Formik } from 'formik';
+import React from 'react';
+import { Button, Grid } from 'semantic-ui-react';
+
+import { DiagnosisSelection, TextField } from '../AddPatientModal/FormField';
+import { useStateValue } from '../state';
+import { Entry } from '../types';
+
+export type EntryFormValues = Omit<Entry, "id">;
+
+interface Props {
+  onSubmit: (values: EntryFormValues) => void;
+  onCancel: () => void;
+}
+
+const AddEntryForm: React.FC<Props> = ({ onSubmit, onCancel }) => {
+  const [{ diagnoses }] = useStateValue();
+
+  return (
+    <Formik
+      initialValues={{
+        date: "",
+        specialist: "",
+        description: "",
+        diagnosisCodes: [],
+        type: "HealthCheck"
+      }}
+      onSubmit={onSubmit}
+      validate={values => {
+        const requiredError = "Field is required";
+        const errors: { [field: string]: string } = {};
+        if (!values.date) {
+          errors.date = requiredError;
+        }
+        if (!values.specialist) {
+          errors.specialist = requiredError;
+        }
+        if (!values.description) {
+          errors.description = requiredError;
+        }
+      }}
+    >
+      {({ isValid, dirty, setFieldValue, setFieldTouched }) => {
+        return (
+          <Form>
+            <Field
+              label="Date"
+              placeholder="YYYY-MM-DD"
+              name="date"
+              component={TextField}
+            />
+            <Field
+              label="Specialist"
+              placeholder="Specialist"
+              name="specialist"
+              component={TextField}
+            />
+            <Field
+              label="Description"
+              placeholder="Description"
+              name="description"
+              component={TextField}
+            />
+            <DiagnosisSelection
+              diagnoses={Object.values(diagnoses)}
+              setFieldValue={setFieldValue}
+              setFieldTouched={setFieldTouched}
+            />
+            <Grid>
+              <Grid.Column floated="left" width={5}>
+                <Button type="button" onClick={onCancel} color="red">
+                  Cancel
+                </Button>
+              </Grid.Column>
+              <Grid.Column floated="right" width={5}>
+                <Button
+                  type="submit"
+                  floated="right"
+                  color="green"
+                  disabled={!dirty || !isValid}
+                >
+                  Add
+                </Button>
+              </Grid.Column>
+            </Grid>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default AddEntryForm;
--- patientor/client/src/PatientInfoPage/index.tsx
+++ patientor/client/src/PatientInfoPage/index.tsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Header, Icon } from 'semantic-ui-react';
+import { Button, Header, Icon } from 'semantic-ui-react';
 
+import AddEntryModal from '../AddEntryModal';
+import { EntryFormValues } from '../AddEntryModal/AddEntryForm';
 import EntryDetails from '../components/EntryDetails';
 import { apiBaseUrl } from '../constants';
 import { setPatient, useStateValue } from '../state';
@@ -11,6 +13,9 @@ import { Patient } from '../types';
 const PatientInfoPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const [{ patient }, dispatch] = useStateValue();
+
+  const [modalOpen, setModalOpen] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | undefined>();
 
   React.useEffect(() => {
     if (patient && patient.id === id) {
@@ -30,6 +35,26 @@ const PatientInfoPage: React.FC = () => {
     fetchPatient();
     // eslint-disable-next-line
   }, [id, dispatch]);
+
+  const openModal = (): void => setModalOpen(true);
+
+  const closeModal = (): void => {
+    setModalOpen(false);
+    setError(undefined);
+  };
+
+  const submitNewEntry = async (values: EntryFormValues) => {
+    try {
+      const { data: updatedPatient } = await axios.post<Patient>(
+        `${apiBaseUrl}/patients/${id}/entries`,
+        values
+      );
+      dispatch(setPatient(updatedPatient));
+      closeModal();
+    } catch(e) {
+      setError(e.response.data.error);
+    }
+  };
 
   if (!patient) {
     return <div>Patient not found!</div>;
@@ -56,6 +81,13 @@ const PatientInfoPage: React.FC = () => {
           No entries at the moment
         </div>
       )}
+      <AddEntryModal
+        modalOpen={modalOpen}
+        onSubmit={submitNewEntry}
+        error={error}
+        onClose={closeModal}
+      />
+      <Button onClick={() => openModal()}>Add New Entry</Button>
       {patient.entries.map(entry => (
         <EntryDetails
           key={entry.id}
