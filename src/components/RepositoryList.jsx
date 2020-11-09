840e677bc4876f6bb8d6ea723eabcb438bb92206
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-11-09 14:29:23 +0000

    Move ItemSeparator component to new file

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import { FlatList, StyleSheet, TouchableOpacity, View } from "react-native";
+import { FlatList, TouchableOpacity } from "react-native";
 import { useHistory } from "react-router-native";
 
 import useRepositories from "../hooks/useRepositories";
+import ItemSeparator from "./ItemSeparator";
 import RepositoryItem from "./RepositoryItem";
-
-const styles = StyleSheet.create({
-  separator: {
-    height: 10,
-  },
-});
-
-const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
   const { repositories } = useRepositories();
