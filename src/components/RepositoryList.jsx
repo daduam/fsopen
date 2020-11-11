39c8dbf9b30a8a3d039d8d126ad8a29669473877
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-11-11 12:56:15 +0000

    add sort repositories select

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,58 @@
 import React from "react";
 import { FlatList, TouchableOpacity } from "react-native";
+import PickerSelect from "react-native-picker-select";
 import { useHistory } from "react-router-native";
 
 import useRepositories from "../hooks/useRepositories";
 import ItemSeparator from "./ItemSeparator";
 import RepositoryItem from "./RepositoryItem";
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
-  const history = useHistory();
+const Dropdown = ({ handleSelect, selectValue }) => {
+  return (
+    <PickerSelect
+      onValueChange={(value) => handleSelect(value)}
+      value={selectValue}
+      style={{ viewContainer: { paddingHorizontal: 8 } }}
+      items={[
+        {
+          label: "Latest repositories",
+          value: {
+            orderBy: "CREATED_AT",
+            orderDirection: undefined,
+          },
+        },
+        {
+          label: "Highest rated repositories",
+          value: {
+            orderBy: "RATING_AVERAGE",
+            orderDirection: "DESC",
+          },
+        },
+        {
+          label: "Lowest rated repositories",
+          value: {
+            orderBy: "RATING_AVERAGE",
+            orderDirection: "ASC",
+          },
+        },
+      ]}
+    />
+  );
+};
 
-  const repositoryNodes = repositories
-    ? repositories.edges.map((edge) => edge.node)
-    : [];
+const RepositoryList = () => {
+  const history = useHistory();
+  const [variables, setVariables] = React.useState(null);
+  const [selectValue, setSelectValue] = React.useState(null);
+  const { repositories } = useRepositories(variables);
 
   const onPress = (id) => {
     history.push(`/repository/${id}`);
   };
+
+  const repositoryNodes = repositories
+    ? repositories.edges.map((edge) => edge.node)
+    : [];
 
   return (
     <FlatList
@@ -26,6 +62,15 @@ const RepositoryList = () => {
         <TouchableOpacity onPress={() => onPress(item.id)}>
           <RepositoryItem key={index} item={item} />
         </TouchableOpacity>
+      )}
+      ListHeaderComponent={() => (
+        <Dropdown
+          handleSelect={(values) => {
+            setVariables({ ...variables, ...values });
+            setSelectValue(values);
+          }}
+          selectValue={selectValue}
+        />
       )}
     />
   );
